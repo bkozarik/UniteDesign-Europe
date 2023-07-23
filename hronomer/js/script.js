c17c3f3405237770a8b3eb68d69902e5d4961391
Bohdan <59025054+bkozarik@users.noreply.github.com>
2023-07-23 21:51:09 +0200

    Add files via upload

--- hronomer/js/script.js
+++ hronomer/js/script.js
@@ -1,33 +1,3 @@
-function MM_swapImgRestore() { 
-	var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
-}
-
-function MM_preloadImages() { 
-	var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
-	var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
-	if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
-}
-
-function MM_findObj(n, d) { 
-	var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
-	d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
-	if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
-	for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
-	if(!x && document.getElementById) x=document.getElementById(n); return x;
-}
-
-function MM_swapImage() { 
-	var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
-	if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
-}
-
-function openWin(tt){
-	var tt_u ="index.php?page="+tt;
-	newwin = window.open(tt_u,"_blank","toolbar=0,scrollbars=no,width=340,height=140,status=no,toolbar=no,menubar=no,resizable=no,alwaysRaised=yes");
-	setTimeout('newwin.focus()',250);
-}
-
-
 function GetWordsEnd(iCount){
 	var sText = "";
 			if ((iCount >= 5) & (iCount <= 20)){
@@ -208,3 +178,6 @@ function activeField(sCheck,sField){
 	eval("if (document.forms.frmHronomer."+sCheck+".checked) document.forms.frmHronomer."+sField+".disabled=false; else document.forms.frmHronomer."+sField+".disabled=true;");
 	return (true);
 }
+
+
+  
